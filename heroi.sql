cf6b675ca710417fc1930a304f0309859dc212a4
ManoelCavenati <140075906+ManoelCavenati@users.noreply.github.com>
2024-04-05 19:28:24 -0300

    EX M2S06 HEROIS

--- heroi.sql
+++ heroi.sql
@@ -1,5 +1,5 @@
 /* Exercício 1*/
-CREATE TABLE heroi (
+CREATE TABLE herois (
     user_id serial PRIMARY KEY,
     username VARCHAR (50) NOT NULL,
     superpoder VARCHAR (200),
@@ -8,23 +8,23 @@ CREATE TABLE heroi (
 );
 
 /* Exercício 2*/
-INSERT INTO heroi (username, superpoder, idade, data_cadastro) VALUES ('Spiderman', 'teia', '18', '02/04/2024');
-INSERT INTO heroi (username, superpoder, idade, data_cadastro) VALUES ('Iron Man', 'armadura', '40', '02/04/2024');
-INSERT INTO heroi (username, superpoder, idade, data_cadastro) VALUES ('Batman', 'bat-coisas', '39', '02/04/2024');
-INSERT INTO heroi (username, superpoder, idade, data_cadastro) VALUES ('Superman', 'super-força', '26', '02/04/2024');
+INSERT INTO herois (username, superpoder, idade, data_cadastro) VALUES ('Spiderman', 'teia', '18', CURRENT_TIMESTAMP);
+INSERT INTO herois (username, superpoder, idade, data_cadastro) VALUES ('Iron Man', 'armadura', '40', CURRENT_TIMESTAMP);
+INSERT INTO herois (username, superpoder, idade, data_cadastro) VALUES ('Batman', 'bat-coisas', '39', CURRENT_TIMESTAMP);
+INSERT INTO herois (username, superpoder, idade, data_cadastro) VALUES ('Superman', 'super-força', '26', CURRENT_TIMESTAMP);
 
 /* Exercício 3*/
-UPDATE heroi SET idade = 41 WHERE username = 'Iron Man';
+UPDATE herois SET idade = 41 WHERE username = 'Iron Man';
 
 /* Exercício 4*/ 
-SELECT * FROM heroi WHERE idade > 30;
+SELECT nome, idade FROM herois WHERE idade > 30;
 
 
 /* Exercício 5*/
-SELECT user_id, username FROM heroi WHERE superpoder IS NULL;
+SELECT user_id, username FROM herois WHERE superpoder IS NULL;
 
 /* Exercício 6*/
-DELETE FROM heroi WHERE username = 'Batman';
+DELETE FROM herois WHERE username = 'Batman';
 
 /* Exercício 7*/
-SELECT * FROM heroi;
+SELECT * FROM herois;
